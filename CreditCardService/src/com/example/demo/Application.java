2c2b1c47db8d722f5af8d7e10dadeae715a30371
Admin <Admin@DESKTOP-F8V312A>
2021-07-12 14:52:06 +0530

    Creating project for Collaboration

---
+++ CreditCardService/src/com/example/demo/Application.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to Credit Card App!");
+	}
+
+}

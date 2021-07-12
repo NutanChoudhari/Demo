faf858e2d248059bf23aa92fc5e902f47f01dc8a
Ayesha <Admin@DESKTOP-F8V312A>
2021-07-12 15:53:17 +0530

    Updated setter and getter methods

--- CreditCardService/src/com/example/demo/Service.java
+++ CreditCardService/src/com/example/demo/Service.java
@@ -4,6 +4,24 @@ public class Service {
 	
 	private String userName;
 	private int id;
-	private int rollNo;
-
+	private long phoneNumber;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 }

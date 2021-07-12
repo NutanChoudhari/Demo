56d15025435bdef7d6812096a39a659db1f4b018
Ayesha <Admin@DESKTOP-F8V312A>
2021-07-12 15:58:41 +0530

    Updated the methods

--- CreditCardService/src/com/example/demo/Service.java
+++ CreditCardService/src/com/example/demo/Service.java
@@ -3,6 +3,7 @@ package com.example.demo;
 public class Service {
 	
 	private String userName;
+	private double creditLimit;
 	private int id;
 	private long phoneNumber;
 	public String getUserName() {
@@ -22,6 +23,12 @@ public class Service {
 	}
 	public void setId(int id) {
 		this.id = id;
+	}
+	public double getCreditLimit() {
+		return creditLimit;
+	}
+	public void setCreditLimit(double creditLimit) {
+		this.creditLimit = creditLimit;
 	}
 	
 }
